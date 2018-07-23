8462b904b7bfcb3cf6d1a178d01592e19beecc05
Mark Francis <mrkfrancis8@gmail.com>
2018-07-23 15:18:40 +0100

    Completed FizzBuzz

--- BasicTraining/FizzBuzzSolution/FizzBuzz.cs
+++ BasicTraining/FizzBuzzSolution/FizzBuzz.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace FizzBuzzSolution
 {
@@ -7,12 +8,30 @@ namespace FizzBuzzSolution
     {
         public static IEnumerable<string> PlayFizzBuzzUpTo(int upTo)
         {
-            throw new NotImplementedException();
+            return PlayFizzBuzz2UpTo(upTo, new Dictionary<int, string>
+            {
+                { 3, "Fizz" },
+                { 5, "Buzz" }
+            });
         }
 
         public static IEnumerable<string> PlayFizzBuzz2UpTo(int upTo, IDictionary<int, string> fizzBuzzSettings)
         {
-            throw new NotImplementedException();
+            var settings = fizzBuzzSettings.OrderBy(s => s.Key).ToList();
+
+            for (int curNum = 1; curNum <= upTo; ++curNum)
+            {
+                StringBuilder val = new StringBuilder();
+                foreach (var setting in settings)
+                {
+                    if (curNum % setting.Key == 0)
+                    {
+                        val.Append(setting.Value);
+                    }
+                }
+
+                yield return val.Length > 0 ? val.ToString() : curNum.ToString();
+            }
         }
     }
 }
