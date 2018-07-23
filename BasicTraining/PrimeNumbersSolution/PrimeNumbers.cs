cb82f87fa02c87653576b2de2dc837c8ac8194d5
Mark Francis <mrkfrancis8@gmail.com>
2018-07-23 15:42:08 +0100

    Completed Prime Numbers

--- BasicTraining/PrimeNumbersSolution/PrimeNumbers.cs
+++ BasicTraining/PrimeNumbersSolution/PrimeNumbers.cs
@@ -7,22 +7,87 @@ namespace PrimeNumbersSolution
     {
         public static IEnumerable<int> GetFactors(int factorsOf)
         {
-            throw new NotImplementedException();
+            if (factorsOf < 0)
+            {
+                throw new NotSupportedException();
+            }
+
+            if (factorsOf == 0)
+            {
+                yield break;
+            }
+
+            yield return 1;
+
+            for (int curVal = 2; curVal <= factorsOf / 2; ++curVal)
+            {
+                if (factorsOf % curVal == 0)
+                {
+                    yield return curVal;
+                }
+            }
+
+            if (factorsOf > 1)
+            {
+                yield return factorsOf;
+            }
         }
 
         public static IEnumerable<int> GetNPrimes(int numberOfPrimesToGet)
         {
-            throw new NotImplementedException();
+            List<int> primes = new List<int>();
+
+            int currentNumber = 0;
+            while (primes.Count < numberOfPrimesToGet)
+            {
+                if (IsAPrimeNumber(++currentNumber, primes))
+                {
+                    primes.Add(currentNumber);
+                }
+            }
+
+            return primes;
         }
 
         public static IEnumerable<int> GetPrimesUpTo(int upTo)
         {
-            throw new NotImplementedException();
+            List<int> primes = new List<int>();
+
+            for (int currentNumber = 2; currentNumber < upTo; ++currentNumber)
+            {
+                if (IsAPrimeNumber(currentNumber, primes))
+                {
+                    primes.Add(currentNumber);
+                }
+            }
+
+            return primes;
         }
 
         public static bool IsAPrimeNumber(int isThisAPrime)
         {
-            throw new NotImplementedException();
+            for (int curVal = 2; curVal <= isThisAPrime / 2; ++curVal)
+            {
+                if (isThisAPrime % curVal == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsAPrimeNumber(int number, List<int> primesBelowThis)
+        {
+            for (int primeIndex = 0; primeIndex < primesBelowThis.Count; ++primeIndex)
+            {
+                if (number % primesBelowThis[primeIndex] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
